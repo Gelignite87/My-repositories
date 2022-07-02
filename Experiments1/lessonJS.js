0eca65fc8399e5544f5fde905cebdd64ea080db7
Gelignite87 <railgun87@rambler.ru>
2022-07-02 20:01:40 +1300

    02.07.2022

--- Experiments1/lessonJS.js
+++ Experiments1/lessonJS.js
@@ -1,27 +1,33 @@
 const products = [
-    { id: 1, title: 'Notebook', price: 20000 },
-    { id: 2, title: 'Mouse', price: 1500 },
-    { id: 3, title: 'Keyboard', price: 5000 },
-    { id: 4, title: 'Gamepad', price: 4500 },
-    { id: 5, title: 'Keyboard', price: 5000 },
+    { id: 1, title: 'Notebook', img: 'Notebook', price: 20000 },
+    { id: 2, title: 'Mouse', img: 'Mouse', price: 1500 },
+    { id: 3, title: 'Keyboard', img: 'Keyboard', price: 5000 },
+    { id: 4, title: 'Gamepad', img: 'Gamepad', price: 4500 },
+    { id: 5, title: 'Microphone', img: 'Microphone', price: 5000 },
 ];
 
-const renderProduct = (title, price) => {
-    return `<div class="block_product">
-        <div><img class="img" src="images/Product1.png" alt="Product1"></div>
+const renderProduct = (title, img, price) =>
+    `<div class="block_product">
+        <div>
+            <img class="img" src="imagesForBasket/${img}.png" alt="${img}">
+        </div>
         <div class="block_text_product">
             <h3 class="block_text_product1">${title}</h3>
             <p class="block_text_product1">${price}</p>
-            <button class="by-btn">Добавить в корзину</button></div>
+            <button class="by-btn">Добавить в корзину</button>
+        </div>
     </div>`;
-};
 
 const renderProducts = list => {
-    const productList = list.map(item => renderProduct(item.title, item.price));
+    const productList = list.map(item => {
+        return renderProduct(item.title, item.img, item.price)
+    });
     document.querySelector('.product').innerHTML = productList.join('');
 }
 
 renderProducts(products);
+
+
 
 document.querySelector('.fon_menu').addEventListener(`click`, btnHenler);
 function btnHenler() {
